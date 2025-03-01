178e4d20dd2b977a1eaa7a23eabddd4dbfd4f568
Ajeet Pal <155215332+Ajeet-Pal7@users.noreply.github.com>
2025-03-01 23:51:45 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,24 +1,27 @@
 require('dotenv').config();
 const express = require('express');
-const path = require('path');
-const cors = require('cors');
-const session = require('express-session');
-const bodyParser = require('body-parser');
-const connectDB = require('./config/database');
+// const path = require('path');
+// const cors = require('cors');
+// const session = require('express-session');
+// const bodyParser = require('body-parser');
+// const connectDB = require('./config/database');
 
 // Initialize app
 const app = express();
+app.get("/test", (req, res) => {
+    res.send("API is working!");
+});
 
 // Connect to database
-connectDB();
+// connectDB();
 
 // Middleware setup
-app.use(express.static(path.join(__dirname, 'public')));
-app.use(express.static('public'));
-app.use(express.json());
-app.use(cors({ origin: "https://internsity-production.up.railway.app", methods: "GET,POST,PUT,DELETE" }));
-app.use(express.urlencoded({ extended: false }));
-app.use(bodyParser.json());
+// app.use(express.static(path.join(__dirname, 'public')));
+// app.use(express.static('public'));
+// app.use(express.json());
+// app.use(cors({ origin: "https://internsity-production.up.railway.app", methods: "GET,POST,PUT,DELETE" }));
+// app.use(express.urlencoded({ extended: false }));
+// app.use(bodyParser.json());
 
 // app.use(session({
 //     secret: process.env.SECRET,
@@ -27,39 +30,39 @@ app.use(bodyParser.json());
 //     cookie: { secure: false, httpOnly: true, maxAge: 30 * 60 * 1000 }
 // }));
 
-app.get('/', async (req, res) => {
-    await otherUtils.sendFileWithFallback(
-                res,
-                path.join(__dirname, '../public', 'index.html'),
-                path.join(__dirname, '../public', 'error-404.html')
-            );
-});
+// app.get('/', async (req, res) => {
+//     await otherUtils.sendFileWithFallback(
+//                 res,
+//                 path.join(__dirname, '../public', 'index.html'),
+//                 path.join(__dirname, '../public', 'error-404.html')
+//             );
+// });
 
-app.get('/pages/about', async (req, res) => {
-    console.log("Function Working");
-    await otherUtils.sendFileWithFallback(
-                res,
-                path.join(__dirname, '../public', 'abiut-us.html'),
-                path.join(__dirname, '../public', 'error-404.html')
-            );
-});
+// app.get('/pages/about', async (req, res) => {
+//     console.log("Function Working");
+//     await otherUtils.sendFileWithFallback(
+//                 res,
+//                 path.join(__dirname, '../public', 'abiut-us.html'),
+//                 path.join(__dirname, '../public', 'error-404.html')
+//             );
+// });
 
 // Import Routes
-app.use('/auth', require('./routes/authRoutes'));
-app.use('/profile', require('./routes/profileRoutes'));
-// app.use('/pages', require('./routes/pageRoutes'));
-app.use('/internships', require('./routes/internshipRoutes'));
-app.use('/portfolio', require('./routes/portfolioRoutes'));
-app.use('/admins', require('./routes/adminRoutes'));
+// app.use('/auth', require('./routes/authRoutes'));
+// app.use('/profile', require('./routes/profileRoutes'));
+// // app.use('/pages', require('./routes/pageRoutes'));
+// app.use('/internships', require('./routes/internshipRoutes'));
+// app.use('/portfolio', require('./routes/portfolioRoutes'));
+// app.use('/admins', require('./routes/adminRoutes'));
 
 // 404 Fallback
-app.use(async (req, res) => {
-    await otherUtils.sendFileWithFallback(
-                res,
-                path.join(__dirname, '../public', 'error-404.html'),
-                path.join(__dirname, '../public', 'error-404.html')
-            );
-});
+// app.use(async (req, res) => {
+//     await otherUtils.sendFileWithFallback(
+//                 res,
+//                 path.join(__dirname, '../public', 'error-404.html'),
+//                 path.join(__dirname, '../public', 'error-404.html')
+//             );
+// });
 
 // Start server
 const PORT = process.env.PORT;
