218a825155818399759c2cee8d38e8901b78e28f
Ajeet Pal <155215332+Ajeet-Pal7@users.noreply.github.com>
2025-03-10 15:03:49 +0530

    Update transporter.js
    SyntaxError resolved

--- transporter.js
+++ transporter.js
@@ -3,7 +3,7 @@ const transporter = nodemailer.createTransport({
     // service: 'gmail',
     host: process.env.SMTP_HOST,
     port: process.env.SMTP_PORT,
-    secure: process.env.SMTP_SECURE
+    secure: process.env.SMTP_SECURE,
     auth: {
         user: process.env.USER_EMAIL,
         pass: process.env.EMAIL_PASS,
