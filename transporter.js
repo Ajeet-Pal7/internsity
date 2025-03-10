4e97a93394763273c55116c9d8434a30d2ae939f
unknown <youranonbhai@hotmail.com>
2025-03-10 14:53:05 +0530

    Email sender address updated

--- transporter.js
+++ transporter.js
@@ -1,8 +1,11 @@
 const nodemailer = require('nodemailer');
 const transporter = nodemailer.createTransport({
-    service: 'gmail',
+    // service: 'gmail',
+    host: process.env.SMTP_HOST,
+    port: process.env.SMTP_PORT,
+    secure: true, // True for 465, false for 587
     auth: {
-        user: process.env.USER_EMAIL, 
+        user: process.env.USER_EMAIL,
         pass: process.env.EMAIL_PASS,
     },
 });
